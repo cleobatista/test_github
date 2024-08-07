a0060c391bd379ab29f7de751b55f7d43b36f597
Cleo Batista <batista.cleo@gmail.com>
2024-08-07 19:27:30 -0300

    first commit

---
+++ example.py
@@ -0,0 +1,13 @@
+def hello_world(name):
+    print(f"Hello, {name}!")
+
+
+x = [1, 2, 3, 4, 5]
+for i in x:
+    if i % 2 == 0:
+        print(i)
+
+y = 10
+if y > 5:
+    print("y is greater than 5")
+    print("This line no longer has trailing whitespace")
